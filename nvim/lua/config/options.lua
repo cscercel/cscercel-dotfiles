cfdfd954fba2e75d2d8f4b45d586534a3d59a36c
Cercel, Cristian <cristian.cercel@outlook.com>
2026-03-22 16:30:54 -0400

    better indent

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,7 +7,7 @@ vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
-vim.opt.autoindent = true
+vim.opt.smartindent = true
 
 -- Search settings
 vim.opt.hlsearch = false
