952d49dcc17521d82b0020fed05c43ec4c704475
Cercel, Cristian <cristian.cercel@outlook.com>
2026-07-01 15:17:20 -0400

    new parsers

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,32 +2,39 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-	    local configs = require("nvim-treesitter.configs")
-	    configs.setup({
-	        highlight = {
-		    enable = true,
-	        },
-	        indent = { enable = true },
-	        autotage = { enable = true },
-	        ensure_installed = {
-		        "lua",
-		        "python",
-		        "toml",
-		        "yaml",
-		        "json",
-		        "html",
-		        "css",
-		        "bash",
-		        "gitignore",
-		        "markdown",
-		        "markdown_inline",
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+            highlight = { enable = true },
+            indent = { enable = true },
+            ensure_installed = {
+                "lua",
+                "python",
+                "toml",
+                "yaml",
+                "json",
+                "html",
+                "css",
+                "bash",
+                "gitignore",
+                "markdown",
+                "markdown_inline",
                 "javascript",
                 "jsdoc",
                 "typescript",
+                "tsx",
                 "go",
                 "sql",
-	        },
-	        auto_install = false,
-	})
+                "templ",
+                "comment",
+                "gomod",
+                "gosum",
+                "gotmpl",
+                "gowork",
+                "query",
+                "vim",
+                "vimdoc",
+            },
+            auto_install = false,
+        })
     end
 }
